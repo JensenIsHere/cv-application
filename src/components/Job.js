50249d0bfcf0f61ff68d926ab628cf7226670a1f
Jensen Ruud <jensen.is.testing@gmail.com>
2022-07-14 15:51:13 -0500

    Styling finished

--- src/components/Job.js
+++ src/components/Job.js
@@ -5,21 +5,41 @@ export default class Education extends React.Component {
   render() {
     const { num } = this.props;
     return (
-      <div>
-        <label htmlFor={"checkbox_job_" + num}>Employer #{num}</label>
+      <div className="job">
+        <label htmlFor={"checkbox_job_" + num} className="label--checkbox">
+          Employer #{num}
+        </label>
         <input id={"checkbox_job_" + num} type="checkbox" />
         <div className={"job_" + num}>
-          <label htmlFor={"name_" + num}>Company:</label>
-          <input id={"name_" + num} type="text" />
-          <label htmlFor={"position_" + num}>position:</label>
-          <input id={"position_" + num} type="text" />
-          <label htmlFor={"duties_" + num}>Job Duties:</label>
-          <textarea id={"duties_" + num}></textarea>
-          <p>Date of Employment:</p>
-          <label htmlFor={"date_" + num + "from"}>From</label>
-          <input id={"date_" + num + "from"} type="text" />
-          <label htmlFor={"date_" + num + "to"}>To</label>
-          <input id={"date_" + num + "to"} type="text" />
+          <label htmlFor={"name_" + num} className="job--label">
+            Company:
+          </label>
+          <input id={"name_" + num} type="text" className="job--input" />
+          <label htmlFor={"position_" + num} className="job--label">
+            Position:
+          </label>
+          <input id={"position_" + num} type="text" className="job--input" />
+          <label htmlFor={"duties_" + num} className="job--label">
+            Job Duties:
+          </label>
+          <textarea id={"duties_" + num} className="job--duties"></textarea>
+          <p className="job--label">Date:</p>
+          <label htmlFor={"date_" + num + "from"} className="job--label_date">
+            From
+          </label>
+          <input
+            id={"date_" + num + "from"}
+            type="date"
+            className="job--input_date"
+          />
+          <label htmlFor={"date_" + num + "to"} className="job--label_date">
+            To
+          </label>
+          <input
+            id={"date_" + num + "to"}
+            type="date"
+            className="job--input_date"
+          />
         </div>
       </div>
     );
--- src/components/Personal.js
+++ src/components/Personal.js
@@ -5,19 +5,31 @@ export default class Personal extends React.Component {
   render() {
     return (
       <div className="personal">
-        <p className="info--heading">Personal Information</p>
-        <label htmlFor="first_name">First Name:</label>
-        <input id="first_name" type="text" />
-        <label htmlFor="last_name">Last Name:</label>
-        <input id="last_name" type="text" />
-        <label htmlFor="age">Age:</label>
-        <input id="age" type="text" />
-        <label htmlFor="gender">Gender:</label>
-        <input id="gender" type="text" />
-        <label htmlFor="email">Email:</label>
-        <input id="email" type="text" />
-        <label htmlFor="phone_num">Phone #:</label>
-        <input id="phone_num" type="text" />
+        <p className="personal heading">Personal Information</p>
+        <label htmlFor="first_name" className="personal--label">
+          First Name:
+        </label>
+        <input id="first_name" type="text" className="personal--input" />
+        <label htmlFor="last_name" className="personal--label">
+          Last Name:
+        </label>
+        <input id="last_name" type="text" className="personal--input" />
+        <label htmlFor="age" className="personal--label">
+          Age:
+        </label>
+        <input id="age" type="text" className="personal--input" />
+        <label htmlFor="gender" className="personal--label">
+          Gender:
+        </label>
+        <input id="gender" type="text" className="personal--input" />
+        <label htmlFor="email" className="personal--label">
+          Email:
+        </label>
+        <input id="email" type="text" className="personal--input" />
+        <label htmlFor="phone_num" className="personal--label">
+          Phone #:
+        </label>
+        <input id="phone_num" type="text" className="personal--input" />
       </div>
     );
   }
